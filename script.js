342e46eb75b26e77ac2a4e54740a9ea02787ab96
santodev <dseosantos76@gmail.com>
2023-02-12 18:17:46 -0400

    leyendo HTML desde JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-var shit = document.getElementById("shit");
+/* leyendo HTML desde JavaScript */
+
+// creamos variables que representen a los selectores de nuestro document
+
+// querySelector seleccionamos el tag, funciona como CSS
+
+const h1 = document.querySelector("h1")
+const p = document.querySelector("p")
+const parrafito = document.querySelector(".parrafito")
+const pid = document.querySelector("#pid")
+const input = document.querySelector("input")
+
+// print object and values in the console
+console.log({
+    h1, p, parrafito, pid, input,
+});
