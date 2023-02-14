a7201b1c329dae02b6742e30912c6480f5f44f49
santodev <dseosantos76@gmail.com>
2023-02-14 06:36:23 -0400

    Escribiendo HTML dsde Javascript

--- script.js
+++ script.js
@@ -5,12 +5,64 @@
 // querySelector seleccionamos el tag, funciona como CSS
 
 const h1 = document.querySelector("h1")
-const p = document.querySelector("p")
-const parrafito = document.querySelector(".parrafito")
-const pid = document.querySelector("#pid")
-const input = document.querySelector("input")
+const p = document.querySelector('p')
+const parrafito = document.getElementsByClassName("parrafito");
+const parrafo_id = document.querySelector("#parrafoId");
+const input = document.querySelector("input");
 
+console.log(parrafito);
 // print object and values in the console
 console.log({
-    h1, p, parrafito, pid, input,
-});
+    h1, p, parrafito, parrafo_id, input
+})
+
+// innerHTML 
+
+h1.innerHTML = "Javaescri";
+
+// tambien esta innerText que solo funciona con strings 
+
+// imprimir un atributo 
+
+console.log(h1.getAttribute("class"))
+
+// modificar el atributo
+
+h1.setAttribute("class", "titulo");
+
+// agregar una clase
+
+h1.classList.add("heading")
+
+// remover una clase
+
+h1.classList.remove("heading");
+
+// devuelve true || false si tenemos esta clase
+
+h1.classList.contains("titulo");
+
+// modificar el value de un input
+
+// input.value = "234";
+
+// console.log(input);
+
+// createElement
+
+const img = document.createElement("img");
+
+img.setAttribute("src", "https://eloquentjavascript.net/img/cover.jpg")
+
+
+console.log(img);
+
+// console.log(img);
+
+parrafo_id.append(img);
+
+// console.log(parrafito);
+
+// reemplazar un elemento
+p.innerHTML = "";
+p.appendChild(img);
