bb4e76e0252efa7fed49772b52695c57c875709e
santodev <dseosantos76@gmail.com>
2023-02-15 22:31:58 -0400

    Eventos en JavaScript: Interactuando con el user

--- script.js
+++ script.js
@@ -1,68 +1,16 @@
-/* leyendo HTML desde JavaScript */
+const h1 = document.querySelector("h1");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector('#result');
 
-// creamos variables que representen a los selectores de nuestro document
+// eventos
 
-// querySelector seleccionamos el tag, funciona como CSS
+function input2Click(){
+    console.log('acaba de pasar algo :)');
+}
 
-const h1 = document.querySelector("h1")
-const p = document.querySelector('p')
-const parrafito = document.getElementsByClassName("parrafito");
-const parrafo_id = document.querySelector("#parrafoId");
-const input = document.querySelector("input");
-
-console.log(parrafito);
-// print object and values in the console
-console.log({
-    h1, p, parrafito, parrafo_id, input
-})
-
-// innerHTML 
-
-h1.innerHTML = "Javaescri";
-
-// tambien esta innerText que solo funciona con strings 
-
-// imprimir un atributo 
-
-console.log(h1.getAttribute("class"))
-
-// modificar el atributo
-
-h1.setAttribute("class", "titulo");
-
-// agregar una clase
-
-h1.classList.add("heading")
-
-// remover una clase
-
-h1.classList.remove("heading");
-
-// devuelve true || false si tenemos esta clase
-
-h1.classList.contains("titulo");
-
-// modificar el value de un input
-
-// input.value = "234";
-
-// console.log(input);
-
-// createElement
-
-const img = document.createElement("img");
-
-img.setAttribute("src", "https://eloquentjavascript.net/img/cover.jpg")
-
-
-console.log(img);
-
-// console.log(img);
-
-parrafo_id.append(img);
-
-// console.log(parrafito);
-
-// reemplazar un elemento
-p.innerHTML = "";
-p.appendChild(img);
+function btnClick(){
+    let resultado = (parseInt(input1.value) + parseInt(input2.value));
+    pResult.innerText = `El resultado es ${resultado}`;
+}
