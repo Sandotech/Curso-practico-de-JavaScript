45df0558bc0f4c5d948b646be0dd767d5274f638
santodev <dseosantos76@gmail.com>
2023-02-17 22:04:53 -0400

    addEventListener event

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const h1 = document.querySelector("h1");
+const form = document.querySelector("form");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
 const pResult = document.querySelector('#result');
+// agregamos adEventListener al elemento seleccionado
+form.addEventListener('submit', SumInputValues);
 
-// eventos
+// event
 
-function input2Click(){
-    console.log('acaba de pasar algo :)');
-}
-
-function btnClick(){
+function SumInputValues(event){
+    console.log({event});
+    //  prevent the default action of an element from happening
+    event.preventDefault();
     let resultado = (parseInt(input1.value) + parseInt(input2.value));
     pResult.innerText = `El resultado es ${resultado}`;
 }
